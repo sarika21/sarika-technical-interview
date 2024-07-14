29f42e51a315dd05ca2abff167c1f274cb307ba7
sarika21 <sarikasingh021@gmail.com>
2024-07-14 11:27:54 +0530

    Delete low-level-design-main/solution/onlineauctionsystem directory
    dd

--- low-level-design-main/solution/onlineauctionsystem/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, user_id, username, email):
-        self.id = user_id
-        self.username = username
-        self.email = email
-
-    def get_id(self):
-        return self.id
